4b3481a543247a4aab78d0ab5ba32e62354904f4
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-06-27 21:20:55 -0300

    feat(sec-05): iota exercise

--- sec-05/main.go
+++ sec-05/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	c0 = iota // c0 == 0
@@ -18,11 +20,28 @@ const (
 func main() {
 	fmt.Println(c0, c1, c2)
 	fmt.Println(c3, c4, c5, c6)
+	
+	const (
+		_ = iota
+		one
+		two
+		three 
+		four 
+		five
+		six
+	)
 	fmt.Printf("%d \t %b\n", 1, 1)
+	fmt.Printf("%d \t %b\n", one, one)
 	fmt.Printf("%d \t %b\n", 1<<1, 1<<1)
+	fmt.Printf("%d \t %b\n", 1<<one, 1<<one)
 	fmt.Printf("%d \t %b\n", 1<<2, 1<<2)
+	fmt.Printf("%d \t %b\n", 1<<two, 1<<two)
 	fmt.Printf("%d \t %b\n", 1<<3, 1<<3)
+	fmt.Printf("%d \t %b\n", 1<<three, 1<<three)
 	fmt.Printf("%d \t %b\n", 1<<4, 1<<4)
+	fmt.Printf("%d \t %b\n", 1<<four, 1<<four)
 	fmt.Printf("%d \t %b\n", 1<<5, 1<<5)
+	fmt.Printf("%d \t %b\n", 1<<five, 1<<five)
 	fmt.Printf("%d \t %b\n", 1<<6, 1<<6)
+	fmt.Printf("%d \t %b\n", 1<<six, 1<<six)
 }
