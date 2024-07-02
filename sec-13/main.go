34b0127e150b1b1cb5aad760b78687cb800239b9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-02 19:56:11 -0300

    feat(sec-13): Add class101 function for slicing a slice

--- sec-13/main.go
+++ sec-13/main.go
@@ -16,6 +16,7 @@ func main() {
 	class98()
 	class99()
 	class100()
+	class101()
 }
 
 func class95() {
@@ -107,4 +108,27 @@ func class100() {
 	fmt.Println(xi)
 	xi = append(xi, 45, 46, 47)
 	fmt.Println(xi)
+}
+
+func class101() {
+	fmt.Println("\nClass 101 - Slice - slicing a slice")
+	xi := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Printf("xi - %#v\n", xi)
+	fmt.Println("-------------------")
+	
+	fmt.Println("[inclusive:exclusive]")
+	fmt.Printf("xi[0:4] - %#v\n", xi[0:4])
+	fmt.Println("-------------------")
+	
+	fmt.Println("[:exclusive] - xi[:7] is the same as xi[0:7]")
+	fmt.Printf("xi[:7] - %#v\n", xi[:7])
+	fmt.Println("-------------------")
+
+	fmt.Println("[inclusive:] - xi[4:] is the same as xi[4:len(xi)]")
+	fmt.Printf("xi[4:] - %#v\n", xi[4:])
+	fmt.Println("-------------------")
+
+	fmt.Println("[:] - xi[:] is the same as xi[0:len(xi)]")
+	fmt.Printf("xi[:] - %#v\n", xi[:])
+	fmt.Println("-------------------")
 }
