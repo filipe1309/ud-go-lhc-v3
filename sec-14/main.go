ef5ed635002466c5332a373971a9615b7ac1a713
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-03 20:09:06 -0300

    feat(sec-14): Add class110 function for slicing a slice

--- sec-14/main.go
+++ sec-14/main.go
@@ -13,6 +13,7 @@ func main() {
 	fmt.Println("Main function")
 	class108()
 	class109()
+	class110()
 }
 
 func class108() {
@@ -35,4 +36,15 @@ func class109() {
 	for i, v := range slice {
 		fmt.Printf("index: %v, value: %v, type: %T\n", i, v, v)
 	}
+}
+
+func class110() {
+	fmt.Println("\nClass 110 - Hands-on exercise #44")
+	slice := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+
+	// slice the slice
+	fmt.Println(slice[:5])
+	fmt.Println(slice[5:])
+	fmt.Println(slice[2:7])
+	fmt.Println(slice[1:6])
 }
