1bf53d10dfaa5973d1ae64c00696fd72882c8a31
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-05 17:02:35 -0300

    feat(sec-19): Add class148 function for working with closures

--- sec-19/main.go
+++ sec-19/main.go
@@ -70,6 +70,7 @@ func main() {
 	class145()
 	class146()
 	class147()
+	class148()
 }
 
 func class133() {
@@ -318,4 +319,30 @@ func add(a int, b int) int {
 
 func sub(a, b int) int {
 	return a - b
+}
+
+
+func class148() {
+	fmt.Println("\nClass 148 - Closure")
+	f := incrementor()
+	fmt.Println(f()) // 1
+	fmt.Println(f()) // 2
+	fmt.Println(f()) // 3
+	fmt.Println(f()) // 4
+	fmt.Println(f()) // 5
+
+	g := incrementor()
+	fmt.Println(g()) // 1
+	fmt.Println(g()) // 2
+	fmt.Println(g()) // 3
+	fmt.Println(g()) // 4
+	fmt.Println(g()) // 5
+}
+
+func incrementor() func() int {
+	x := 0
+	return func () int {
+		x++
+		return x
+	}
 }
