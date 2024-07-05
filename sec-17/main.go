13336cac1a1d886f298d88e9157c5312385d87b8
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-04 22:52:18 -0300

    feat(sec-17): Add class126 function for working with anonymous structs

--- sec-17/main.go
+++ sec-17/main.go
@@ -24,6 +24,7 @@ func main() {
 	fmt.Println("Main function")
 	class124()
 	class125()
+	class126()
 }
 
 func class124() {
@@ -68,4 +69,25 @@ func class125() {
 	fmt.Printf("Type: %T \t Value: %#v\n", sa1, sa1)
 	fmt.Printf("sa1.first: %v, sa1.last: %v, sa1.age: %v, sa1.ltk: %v\n",sa1.first, sa1.last, sa1.age, sa1.ltk)
 	fmt.Printf("sa1.person.first: %v, sa1.person.last: %v, sa1.person.age: %v, sa1.ltk: %v\n",sa1.person.first, sa1.person.last, sa1.person.age, sa1.ltk)
+}
+
+func class126() {
+	fmt.Println("\nClass 124 - Anonymous structs")
+	p1 := struct { // anonymous struct
+		first string
+		last string
+		age int
+	}{
+		first: "James",
+		last: "Bond",
+		age: 42,
+	}
+	p2 := person{
+		first: "Jenny",
+		last: "Moneypenny",
+		age: 27,
+	}
+
+	fmt.Printf("Type: %T \t Value: %#v\n", p1, p1)
+	fmt.Printf("Type: %T \t\t\t\t\t Value: %#v\n", p2, p2)
 }
