fcdd59e8854939343b3d76c6e95d129f8a2d69a1
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-05 00:47:24 -0300

    feat(sec-18): Add class131 function for working with anonymous struct

--- sec-18/main.go
+++ sec-18/main.go
@@ -32,6 +32,7 @@ func main() {
 	class128()
 	class129()
 	class130()
+	class131()
 }
 
 func class128() {
@@ -119,3 +120,29 @@ func class130() {
 	fmt.Printf("Engine: %v, Make: %v, Model: %v, Doors: %v, Color: %v\n", v1.electric, v1.make, v1.model, v1.doors, v1.color)
 	fmt.Printf("Engine: %v, Make: %v, Model: %v, Doors: %v, Color: %v\n", v2.engine.electric, v2.make, v2.model, v2.doors, v2.color)
 }
+
+func class131() {
+	fmt.Println("\nClass 131 - Hands-on exercise #55 - anonymous struct")
+
+	p1 := struct {
+		fisrt string
+		friends map[string]int
+		favDrinks []string
+	}{
+		fisrt: "John",
+		friends: map[string]int{"Elvis": 38, "James": 42},
+		favDrinks: []string{"Martini", "Vodka"},
+	}
+
+	fmt.Println(p1)
+
+	for k, v := range p1.friends {
+		// fmt.Printf("Key: %v \t Value: %v\n", k, v)
+		fmt.Println("Key:", k, "\t Value: ", v)
+	}
+
+	for i, v := range p1.favDrinks {
+		fmt.Printf("Index: %v \t Value: %v\n", i, v)
+	}
+}
+
