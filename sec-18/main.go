b4140cdc7c026710a8f924f1873202282569beff
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-05 00:27:32 -0300

    feat(sec-18): Add class129 function for working with maps and structs

--- sec-18/main.go
+++ sec-18/main.go
@@ -18,6 +18,7 @@ func init() {
 func main() {
 	fmt.Println("Main function")
 	class128()
+	class129()
 }
 
 func class128() {
@@ -45,5 +46,33 @@ func class128() {
 
 	for _, v := range p2.flavors {
 		fmt.Println(v)
+	}
+}
+
+func class129() {
+	fmt.Println("\nClass 129 - Hands-on exercise #54 - map struct")
+
+	p1 := person{
+		first: "John",
+		last: "Doe",
+		flavors: []string{"vanilla", "cream"},
+	}
+
+	p2 := person{
+		first: "Elvis",
+		last: "Presley",
+		flavors: []string{"chocolate", "strwberry", "lemon"},
+	}
+
+	m := map[string]person{
+		p1.last: p1,
+		p2.last: p2,
+	}
+
+	for k, v := range m {
+		fmt.Printf("Key: %v\n", k)
+		for _, v2 := range v.flavors {
+			fmt.Printf("\t Value: %v\n", v2)
+		}
 	}
 }
