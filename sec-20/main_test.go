6b6ef273bfc260028553a9bcca3477580df5e899
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-14 22:36:14 -0300

    feat(sec-20): Add class162 function for mock testing a db

--- sec-20/main_test.go
+++ sec-20/main_test.go
@@ -53,3 +53,23 @@ func TestParadise(t *testing.T) {
 		log.Fatalf("error - want: %v, got: %v", want, got)
 	}
 }
+
+func TestGetUser(t *testing.T) {
+	md := &MockDatastore{
+		Users: map[int]User{
+			2: {ID: 2, First: "Jenny"},
+		},
+	}
+	s := &Service{
+		ds: md,
+	}
+
+	u, err := s.GetUser(2)
+	if err != nil {
+		t.Errorf("got error: %v", err)
+	}
+
+	if u.First != "Jenny" {
+		t.Errorf("got: %s, want: %s", u.First, "Jenny")
+	}
+}
