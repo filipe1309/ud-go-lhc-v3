61ebce829f3f9c3dbe7d4af763b38daf09e73905
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-15 12:57:29 -0300

    feat(sec-21): Add class170 function for working with pointers types in Go

--- sec-21/main.go
+++ sec-21/main.go
@@ -12,11 +12,21 @@ func init() {
 func main() {
 	fmt.Println("Main function")
 	class169()
+	class170()
 }
 
 func class169() {
-	fmt.Println("\nClass 169 - Waht are pointers?")
+	fmt.Println("\nClass 169 - What are pointers?")
 	x := 42
 	fmt.Println(x) // x value
 	fmt.Println(&x) // x address
+}
+
+func class170() {
+	fmt.Println("\nClass 170 - Seeing type & value for pointers?")
+	x := 42
+	fmt.Printf("x value: %v, x type: %T\n", &x, &x)
+
+	y := "James Bond"
+	fmt.Printf("y value: %v, y type: %T\n", &y, &y)
 }
