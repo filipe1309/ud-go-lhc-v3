5d6467ff3693668c5e813395fe244e851486e134
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-15 22:28:31 -0300

    feat(sec-22): Add class179 function for derefering an address

--- sec-22/main.go
+++ sec-22/main.go
@@ -12,6 +12,7 @@ func init() {
 func main() {
 	fmt.Println("Main function")
 	class178()
+	class179()
 }
 
 func class178() {
@@ -19,3 +20,28 @@ func class178() {
 	x := 42
 	fmt.Printf("x value: %v, x type: %T, x address: %v\n", x, x, &x)
 }
+
+
+var (
+	a, b, c *string
+	d       *int
+)
+
+
+func class179() {
+	fmt.Println("\nClass 179 - Hands-on exercise #75 - dereference an address")
+	p := "Drop by drop, the bucket gets filled."
+	q := "Persistently, patiently, you are bound to succeed."
+	r := "The meaning of life is ..."
+	n := 42
+	a = &p
+	b = &q
+	c = &r
+	d = &n
+
+	fmt.Printf("a value: %v, a type: %T, a content: %v\n", a, a, *a)
+	fmt.Printf("b value: %v, b type: %T, b content: %v\n", b, b, *b)
+	fmt.Printf("c value: %v, c type: %T, c content: %v\n", c, c, *c)
+	fmt.Printf("d value: %v, d type: %T,\t d content: %v\n", d, d, *d)
+}
+
