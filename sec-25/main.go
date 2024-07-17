2c359133ef990d10c03e77836d8718dcbbed554a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-17 17:58:13 -0300

    feat(sec-25): Add class196 function for JSON unmarshaling
    Add a new function, class196, to demonstrate JSON unmarshaling in Go. This function unmarshals a JSON string into a slice of person structs using the encoding/json package and prints the parsed data. The purpose of this addition is to showcase the usage of JSON unmarshaling in Go and enhance understanding of working with JSON data.

--- sec-25/main.go
+++ sec-25/main.go
@@ -13,6 +13,7 @@ func init() {
 func main() {
 	fmt.Println("Main function")
 	class195()
+	class196()
 }
 
 type user struct {
@@ -40,4 +41,34 @@ func class195() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(xb))
+}
+
+type person struct {
+	First   string   `json:"First"`
+	Last    string   `json:"Last"`
+	Age     int      `json:"Age"`
+	Sayings []string `json:"Sayings"`
+}
+
+func class196() {
+	fmt.Println("\nClass 196 - Hands-on exercise #2")
+	peopleJson := `[{"First":"James","Last":"Bond","Age":32, "Sayings":["Shaken, not stirred", "Youth is no guarantee of innovation", "In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27, "Sayings":["James! It is so good to see you!", "Would you like me to take care of that for you, James?"]}]`
+	fmt.Println(peopleJson)
+
+	var people []person
+	err := json.Unmarshal([]byte(peopleJson), &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(people)
+
+	for i, p := range people {
+		fmt.Println("Person", i)
+		fmt.Println("\tFirst:", p.First)
+		fmt.Println("\tLast:", p.Last)
+		fmt.Println("\tAge:", p.Age)
+		for j, s := range p.Sayings {
+			fmt.Println("\tSaying", j, ":", s)
+		}
+	}
 }
