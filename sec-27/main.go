900c97c83064d96b9cc2c6cedd9bb010126fd1d5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-18 10:38:31 -0300

    feat(sec-27): Add class208 function for demonstrating interface & method set usage

--- sec-27/main.go
+++ sec-27/main.go
@@ -14,6 +14,7 @@ func init() {
 func main() {
 	fmt.Println("Main function")
 	class207()
+	class208()
 }
 
 func class207() {
@@ -36,4 +37,36 @@ func class207() {
 	fmt.Println("GoRoutines after launch\t", runtime.NumGoroutine())
 	wg.Wait()
 	fmt.Println("GoRoutines after wg.Wait()\t", runtime.NumGoroutine())
+}
+
+type person struct {
+	first string
+}
+
+func (p *person) speak() {
+	fmt.Println("Hello", p.first)
+}
+
+type human interface {
+	speak()
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
+func class208() {
+	fmt.Println("\nClass 208 - Hands-on exercise #2")
+	p1 := person{
+		first: "James",
+	}
+	saySomething(&p1)
+	// saySomething(p1) // This will not work, because p1 is not a pointer
+	p1.speak() // This will work, because is not using the interface
+
+	p2 := &person{
+		first: "John",
+	}
+	saySomething(p2) // This will work, because p2 is a pointer
+	p2.speak()
 }
