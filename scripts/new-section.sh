05420c9d2e970e4244b8ad240fa8c884794e082c
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-22 20:42:37 -0300

    feat: Add description field into new-section script

--- scripts/new-section.sh
+++ scripts/new-section.sh
@@ -9,23 +9,38 @@ NC="\033[0m"
 # get section number from parameter
 SEC=$1
 
+# get description from parameter
+DESC=$2
+
 # if no section number is provided, get the last section number
 if [ -z "$SEC" ]; then
-  SEC=$(ls -d sec-* | sort -n | tail -n 1 | sed 's/[^0-9]*//g')
+  # SEC=$(ls -d sec-* | tail -n 1 | sed 's/[^0-9]{2}//g')
+  SEC=$(ls -d sec-* | sort -n | tail -n 1 | cut -c 5- | cut -d '-' -f 1) # first 2 numbers of the last section
   SEC=$((SEC + 1))
   echo -e "${DARKGRAY}${BOLD}No section number provided. Using $SEC${NC}"
 fi
 
-echo -e "${BOLD}📝 Creating section $SEC${NC}"
+# if no description is provided, ask for it
+if [ -z "$DESC" ]; then
+  read -p "Description: " DESC
+fi
+
+echo -e "${BOLD}📝 Creating section $SEC - $DESC${NC}"
+
+# convert DESC to lowercase and replace spaces with dashes
+DESC_SNAKE_CASE=$(echo $DESC | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
 
 # create a new section directory
-mkdir sec-$SEC
-touch sec-$SEC/main.go
+mkdir sec-$SEC-$DESC_SNAKE_CASE
+touch sec-$SEC-$DESC_SNAKE_CASE/main.go
 
 # create a new section from template
-cp scripts/main sec-$SEC/main.go
+cp scripts/main sec-$SEC-$DESC_SNAKE_CASE/main.go
 
 # replace {{ SEC }} with section number
-sed -i '' "s/{{ SEC }}/$SEC/g" sec-$SEC/main.go
+sed -i '' "s/{{ SEC }}/$SEC/g" sec-$SEC-$DESC_SNAKE_CASE/main.go
 
-echo -e "${BOLD}✅ Section $SEC created${NC}"
+# replace {{ DESC }} with description
+sed -i '' "s/{{ DESC }}/$DESC/g" sec-$SEC-$DESC_SNAKE_CASE/main.go
+
+echo -e "${BOLD}✅ Section $SEC-$DESC_SNAKE_CASE created${NC}"
