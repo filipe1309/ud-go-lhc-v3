1f7709b342de9e645e8e5b86c7ca1967a50fefc4
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-23 12:42:58 -0300

    feat(sec-34): Add table tests for mySum function
    This commit adds table tests for the `mySum` function in the `main_test.go` file. The table tests include multiple test cases with different input values to ensure the correctness of the `mySum` function. This addition enhances the testing coverage and reliability of the codebase.

--- sec-34-testing-and-benchmarking/main_test.go
+++ sec-34-testing-and-benchmarking/main_test.go
@@ -5,9 +5,32 @@ import (
 )
 
 func TestMySum(t *testing.T) {
-	result := mySum(2, 3)
+	got := mySum(2, 3)
 	expected := 5
-	if result != expected {
-		t.Error("Expected", expected, "got", result)
+	if got != expected {
+		t.Error("Expected", expected, "got", got)
+	}
+}
+
+func TestMySumWithTableTestsClass246(t *testing.T) {
+	type test struct {
+		data   []int
+		answer int
+	}
+
+	tests := []test{
+		// test{[]int{21, 21}, 42},
+		{[]int{21, 21}, 42},
+		{[]int{2, 3}, 5},
+		{[]int{4, 7}, 11},
+		{[]int{5, 9, 1}, 15},
+		{[]int{-1, 0, 1}, 0},
+	}
+
+	for _, v := range tests {
+		result := mySum(v.data...)
+		if result != v.answer {
+			t.Error("Expected", v.answer, "got", result)
+		}
 	}
 }
