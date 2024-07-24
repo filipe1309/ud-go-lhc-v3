b12effb9b80dd3894fe6d6055718bffbc72adb22
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-24 09:42:48 -0300

    feat(sec-38): Add concurrency to class258 function
    The commit adds concurrency to the `class258` function in the `main.go` file. It introduces the `foo2` and `bar2` functions, which are executed concurrently using goroutines. The `foo2` function prints numbers with a sleep duration of 3 milliseconds, while the `bar2` function prints numbers with a sleep duration of 20 milliseconds. The `class258WithConcurrency` function uses a `sync.WaitGroup` to wait for the goroutines to complete. This addition enhances the codebase by demonstrating the use of concurrency in Go programming.

---
+++ sec-38-concurrency/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func init() {
+	fmt.Println("Init function")
+	fmt.Println("Section 38 - Concurrency")
+}
+
+func main() {
+	fmt.Println("Main function")
+	class258()
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+}
+
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
+
+func class258NoConcurrency() {
+	foo()
+	bar()
+}
+
+var wg sync.WaitGroup
+
+func foo2() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+		time.Sleep(time.Duration(3 * time.Millisecond))
+	}
+	wg.Done()
+}
+
+func bar2() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+		time.Sleep(time.Duration(20 * time.Millisecond))
+	}
+	wg.Done()
+}
+
+func class258WithConcurrency() {
+	wg.Add(2)
+	go foo2()
+	go bar2()
+	wg.Wait()
+}
+
+func class258() {
+	fmt.Println("\nClass 258 - Concurrency & WaitGroup")
+
+	fmt.Println("\nNo Concurrency")
+	class258NoConcurrency()
+
+	fmt.Println("\nWith Concurrency")
+	class258WithConcurrency()
+}
