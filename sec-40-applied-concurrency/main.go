3b0441be3ad4b903501f0f24ba44518a517e48e9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-07-25 17:52:37 -0300

    feat(sec-40): Add fanIn function for combining multiple boring functions

--- sec-40-applied-concurrency/main.go
+++ sec-40-applied-concurrency/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func init() {
@@ -17,6 +19,7 @@ func main() {
 	class274()
 	class275()
 	class276()
+	class279()
 }
 
 func incrementor(s string) chan int {
@@ -196,4 +199,39 @@ func class276() {
 	for v := range f {
 		fmt.Println(v)
 	}
+}
+
+func boring(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			c <- <-input1
+		}
+	}()
+	go func() {
+		for {
+			c <- <-input2
+		}
+	}()
+	return c
+}
+
+func class279() {
+	fmt.Println("\nClass 279 - Fan In Pattern")
+	c := fanIn(boring("Joe"), boring("Ann"))
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-c)
+	}
+	fmt.Println("You're both boring; I'm leaving.")
 }
